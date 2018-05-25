a84b1a98f1ed3b0fa8ac989f53eaf497b8f67f50
Carolina Escobar <carolinaescobar1997@hotmail.com>
2018-05-25 11:31:50 -0500

    servidor

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ express = require ("express"),
 consolidate = require("consolidate"),
 handlebars = require("handlebars");
 
+
 var app = express();
 
 app.engine('hbs', consolidate.handlebars);
@@ -11,3 +12,10 @@ app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 
+app.get('/', (req, res)=>{
+    res.render("index", {
+         
+    });
+});
+
+app.listen(3000, console.log ("conectado"));
